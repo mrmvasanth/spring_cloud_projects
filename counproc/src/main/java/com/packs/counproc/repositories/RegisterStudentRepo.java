df230321632179992e8719d8ebc422a31e65974a
Vasanth Muralikrishnan <42834098+mrmvasanth@users.noreply.github.com>
2019-07-18 18:55:13 +0530

    Initial Commit

--- counproc/src/main/java/com/packs/counproc/repositories/RegisterStudentRepo.java
+++ counproc/src/main/java/com/packs/counproc/repositories/RegisterStudentRepo.java
@@ -1,6 +1,8 @@
 package com.packs.counproc.repositories;
 
 import com.packs.counproc.models.RegisterModel.RegisterStudent;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,7 +10,7 @@ import java.util.List;
 
 @Transactional
 public interface RegisterStudentRepo extends MongoRepository<RegisterStudent,Integer> {
-    List<RegisterStudent> findByCalled(boolean called);
+    List<RegisterStudent> findByCalled(boolean called, Sort sort);
 
 
 }
--- counproc/src/main/java/com/packs/counproc/services/RegisterServices.java
+++ counproc/src/main/java/com/packs/counproc/services/RegisterServices.java
@@ -4,6 +4,8 @@ import com.packs.counproc.models.RegisterModel.RegisterStudent;
 import com.packs.counproc.models.responses.ApiResponse;
 import com.packs.counproc.repositories.RegisterStudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -34,16 +36,17 @@ public class RegisterServices {
     }
 
     public ApiResponse sendEmails(){
+        // define sort properties to sord by mark percentage
+        Sort sort=new Sort(Sort.Direction.DESC, "marksPercentage");
+
+        Pageable limit = PageRequest.of(0,3);
         // get students with called status=false
-        List<RegisterStudent> registerStudents=registerStudentRepo.findByCalled(false);
-
-        // sort desc based on mark percentage
-
+        List<RegisterStudent> registerStudents=registerStudentRepo.findByCalled(false,sort);
 
         // get top 3 students mail and send mail
 
         // change status to true
 
-        return new ApiResponse(200,HttpStatus.OK,"Emails sent");
+        return new ApiResponse(200,HttpStatus.OK,registerStudents,"Emails sent");
     }
 }
